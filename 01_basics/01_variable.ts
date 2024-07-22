02874c0cc30b13ccaa54a1a5f7c2d9ef5f28f864
Vikesh Gupta <61662892+vikesh107@users.noreply.github.com>
2024-07-22 06:59:25 +0000

    study about varibale and scops

---
+++ 01_basics/01_variable.ts
@@ -0,0 +1,14 @@
+const accountID = 144553
+let accountEmail = "vikesh107@gmail.com"
+var accountPassoword = "12345"
+accountCity = "Raipur"
+
+// accountID = 2; ( not allowed)
+accountEmail = "rinkesh@gmail.com"
+accountPassoword = "3456"
+accountCity = "Jaipur"
+console.log(accountID);
+
+
+let accountState; // this as a undefined;
+console.table([accountID,accountEmail,accountPassoword,accountCity,accountState]);
