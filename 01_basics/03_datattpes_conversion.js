d2bd2374551a24e53a34f85f005950ee70bc027a
Vikesh Gupta <61662892+vikesh107@users.noreply.github.com>
2024-07-22 07:28:19 +0000

    conversion of datta

---
+++ 01_basics/03_datattpes_conversion.js
@@ -0,0 +1,23 @@
+let score = null;
+console.log(typeof score)
+
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1
+
+let isLogedIn = "";
+let boolInLoggedIn = Boolean(isLogedIn);
+console.log(boolInLoggedIn);
+
+// isLogedIn = "" false
+// isLogedIn = "vikesh" true
+
+let name = true
+let someString = String(name);
+console.log(someString);
+console.log(typeof(someString));
