64de71f858849bd55af7a184a3da905dda1a7355
Vikesh Gupta <61662892+vikesh107@users.noreply.github.com>
2024-07-22 07:15:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //teart all js code as newer version
+
+// alert(3+3) we are using not js not browser
+
+console.log("Vikesh");
+console.log("Gupta");
+
+let name = "vikesh";
+let age = 18;
+let isLoggedIn = false;
+ 
